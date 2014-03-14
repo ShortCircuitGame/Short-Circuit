1e7eac3e382de059159f86665c61a6cd5db370b5
Cesar Ramirez <cesar.ramsan@gmail.com>
2014-03-14 16:20:55 -0500

    Added small UI fixes

--- SC-Net/src/shortcircuit/client/GUI.java
+++ SC-Net/src/shortcircuit/client/GUI.java
@@ -22,133 +22,208 @@ import java.awt.event.ActionEvent;
 
 public class GUI extends JFrame implements ClientEventListener {
 
-    private JPanel contentPane;
-    private JTextField roomNameImput;
-    private JTextField textInput;
-    private ShortCircuitClient client;
-    private JTextArea textArea;
-    private JList userList;
-    private JList roomList;
-    
-    /**
-     * Create the frame.
-     * 
-     * @throws IOException
-     */
-    public GUI(final ShortCircuitClient client) throws IOException {
-	setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-	setBounds(100, 100, 516, 358);
-	contentPane = new JPanel();
-	contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-	setContentPane(contentPane);
+	private JPanel contentPane;
+	private JTextField roomNameImput;
+	private JTextField textInput;
+	private ShortCircuitClient client;
+	private JTextArea textArea;
+	private JList userList;
+	private JList roomList;
 
-	roomList = new JList();
-	roomList.setBorder(new LineBorder(new Color(0, 0, 0)));
+	/**
+	 * Create the frame.
+	 * 
+	 * @throws IOException
+	 */
+	public GUI(final ShortCircuitClient client) throws IOException {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 516, 358);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
 
-	JButton createRoomButton = new JButton("Create Room");
+		roomList = new JList();
+		roomList.setBorder(new LineBorder(new Color(0, 0, 0)));
 
-	roomNameImput = new JTextField();
-	roomNameImput.setColumns(10);
+		JButton createRoomButton = new JButton("Create Room");
 
-	userList = new JList();
-	userList.setBorder(new LineBorder(new Color(0, 0, 0)));
+		roomNameImput = new JTextField();
+		roomNameImput.setColumns(10);
 
-	JButton startButton = new JButton("Start");
+		userList = new JList();
+		userList.setBorder(new LineBorder(new Color(0, 0, 0)));
 
-	textArea = new JTextArea();
-	textArea.setBackground(Color.LIGHT_GRAY);
+		JButton startButton = new JButton("Start");
 
-	textInput = new JTextField();
-	textInput.setColumns(10);
+		textArea = new JTextArea();
+		textArea.setBackground(Color.LIGHT_GRAY);
 
-	JButton sendButton = new JButton("Send");
-	sendButton.addActionListener(new ActionListener() {
-		public void actionPerformed(ActionEvent e) {
-		    String message = textInput.getText();
-		    if(!message.isEmpty()){
-			client.sendMessage(new Command(Command.CommandType.CHAT, message));
-		    }
-		}
-	});
-	GroupLayout gl_contentPane = new GroupLayout(contentPane);
-	gl_contentPane.setHorizontalGroup(gl_contentPane.createParallelGroup(Alignment.LEADING).addGroup(
+		textInput = new JTextField();
+		textInput.setColumns(10);
+
+		JButton sendButton = new JButton("Send");
+		sendButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				String message = textInput.getText();
+				if (!message.isEmpty()) {
+					client.sendMessage(new Command(Command.CommandType.CHAT,
+							message));
+					textInput.setText("");
+				}
+			}
+		});
+		GroupLayout gl_contentPane = new GroupLayout(contentPane);
 		gl_contentPane
-			.createSequentialGroup()
-			.addGroup(
-				gl_contentPane
-					.createParallelGroup(Alignment.LEADING, false)
-					.addComponent(roomList, GroupLayout.PREFERRED_SIZE, 125, GroupLayout.PREFERRED_SIZE)
-					.addGroup(
-						gl_contentPane
-							.createParallelGroup(Alignment.TRAILING, false)
-							.addComponent(createRoomButton, Alignment.LEADING, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE,
-								Short.MAX_VALUE).addComponent(roomNameImput, Alignment.LEADING)))
-			.addGap(18)
-			.addGroup(
-				gl_contentPane
-					.createParallelGroup(Alignment.LEADING)
-					.addGroup(
-						gl_contentPane.createSequentialGroup().addComponent(textInput, GroupLayout.DEFAULT_SIZE, 161, Short.MAX_VALUE)
-							.addPreferredGap(ComponentPlacement.RELATED).addComponent(sendButton))
-					.addComponent(textArea, GroupLayout.DEFAULT_SIZE, 237, Short.MAX_VALUE))
-			.addGap(18)
-			.addGroup(
-				gl_contentPane.createParallelGroup(Alignment.TRAILING, false)
-					.addComponent(userList, GroupLayout.PREFERRED_SIZE, 106, GroupLayout.PREFERRED_SIZE).addComponent(startButton))));
-	gl_contentPane.setVerticalGroup(gl_contentPane.createParallelGroup(Alignment.TRAILING).addGroup(
+				.setHorizontalGroup(gl_contentPane
+						.createParallelGroup(Alignment.LEADING)
+						.addGroup(
+								gl_contentPane
+										.createSequentialGroup()
+										.addGroup(
+												gl_contentPane
+														.createParallelGroup(
+																Alignment.LEADING,
+																false)
+														.addComponent(
+																roomList,
+																GroupLayout.PREFERRED_SIZE,
+																125,
+																GroupLayout.PREFERRED_SIZE)
+														.addGroup(
+																gl_contentPane
+																		.createParallelGroup(
+																				Alignment.TRAILING,
+																				false)
+																		.addComponent(
+																				createRoomButton,
+																				Alignment.LEADING,
+																				GroupLayout.DEFAULT_SIZE,
+																				GroupLayout.DEFAULT_SIZE,
+																				Short.MAX_VALUE)
+																		.addComponent(
+																				roomNameImput,
+																				Alignment.LEADING)))
+										.addGap(18)
+										.addGroup(
+												gl_contentPane
+														.createParallelGroup(
+																Alignment.LEADING)
+														.addGroup(
+																gl_contentPane
+																		.createSequentialGroup()
+																		.addComponent(
+																				textInput,
+																				GroupLayout.DEFAULT_SIZE,
+																				161,
+																				Short.MAX_VALUE)
+																		.addPreferredGap(
+																				ComponentPlacement.RELATED)
+																		.addComponent(
+																				sendButton))
+														.addComponent(
+																textArea,
+																GroupLayout.DEFAULT_SIZE,
+																237,
+																Short.MAX_VALUE))
+										.addGap(18)
+										.addGroup(
+												gl_contentPane
+														.createParallelGroup(
+																Alignment.TRAILING,
+																false)
+														.addComponent(
+																userList,
+																GroupLayout.PREFERRED_SIZE,
+																106,
+																GroupLayout.PREFERRED_SIZE)
+														.addComponent(
+																startButton))));
 		gl_contentPane
-			.createSequentialGroup()
-			.addGroup(
-				gl_contentPane
-					.createParallelGroup(Alignment.TRAILING)
-					.addComponent(textArea, GroupLayout.DEFAULT_SIZE, 291, Short.MAX_VALUE)
-					.addGroup(
-						gl_contentPane
-							.createSequentialGroup()
-							.addComponent(roomList, GroupLayout.DEFAULT_SIZE, 266, Short.MAX_VALUE)
-							.addPreferredGap(ComponentPlacement.RELATED)
-							.addComponent(roomNameImput, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE,
-								GroupLayout.PREFERRED_SIZE))
-					.addComponent(userList, GroupLayout.DEFAULT_SIZE, 291, Short.MAX_VALUE))
-			.addPreferredGap(ComponentPlacement.RELATED)
-			.addGroup(
-				gl_contentPane.createParallelGroup(Alignment.BASELINE).addComponent(createRoomButton).addComponent(startButton)
-					.addComponent(textInput, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
-					.addComponent(sendButton))));
-	contentPane.setLayout(gl_contentPane);
+				.setVerticalGroup(gl_contentPane
+						.createParallelGroup(Alignment.TRAILING)
+						.addGroup(
+								gl_contentPane
+										.createSequentialGroup()
+										.addGroup(
+												gl_contentPane
+														.createParallelGroup(
+																Alignment.TRAILING)
+														.addComponent(
+																textArea,
+																GroupLayout.DEFAULT_SIZE,
+																291,
+																Short.MAX_VALUE)
+														.addGroup(
+																gl_contentPane
+																		.createSequentialGroup()
+																		.addComponent(
+																				roomList,
+																				GroupLayout.DEFAULT_SIZE,
+																				266,
+																				Short.MAX_VALUE)
+																		.addPreferredGap(
+																				ComponentPlacement.RELATED)
+																		.addComponent(
+																				roomNameImput,
+																				GroupLayout.PREFERRED_SIZE,
+																				GroupLayout.DEFAULT_SIZE,
+																				GroupLayout.PREFERRED_SIZE))
+														.addComponent(
+																userList,
+																GroupLayout.DEFAULT_SIZE,
+																291,
+																Short.MAX_VALUE))
+										.addPreferredGap(
+												ComponentPlacement.RELATED)
+										.addGroup(
+												gl_contentPane
+														.createParallelGroup(
+																Alignment.BASELINE)
+														.addComponent(
+																createRoomButton)
+														.addComponent(
+																startButton)
+														.addComponent(
+																textInput,
+																GroupLayout.PREFERRED_SIZE,
+																GroupLayout.DEFAULT_SIZE,
+																GroupLayout.PREFERRED_SIZE)
+														.addComponent(
+																sendButton))));
+		contentPane.setLayout(gl_contentPane);
 
-	this.client = client;
-	this.client.addListener(this);
-    }
+		this.client = client;
+		this.client.addListener(this);
+	}
 
-    @Override
-    public void commandRecievedEvent(final Command command) {
-	EventQueue.invokeLater(new Runnable() {
-	    public void run() {
-		switch (command.command) {
-		case CHAT:
-		    textArea.append(command.message+"\n");
-		    break;
-		case CREATE:
-		    break;
-		case JOIN:
-		    break;
-		case LEAVE:
-		    break;
-		case DISCONNECT:
-		    break;
-		case ROOMS:
-		    break;
-		case USERS:
-		    break;
-		case START:
-		    break;
-		case STOP:
-		    break;
-		default:
-		    break;
-		}
-	    }
-	});
-    }
+	@Override
+	public void commandRecievedEvent(final Command command) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				switch (command.command) {
+				case CHAT:
+					textArea.append(command.message + "\n");
+					break;
+				case CREATE:
+					break;
+				case JOIN:
+					break;
+				case LEAVE:
+					break;
+				case DISCONNECT:
+					break;
+				case ROOMS:
+					break;
+				case USERS:
+					break;
+				case START:
+					break;
+				case STOP:
+					break;
+				default:
+					break;
+				}
+			}
+		});
+	}
 }
